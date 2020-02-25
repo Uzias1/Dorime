9f8375edc7876cc2a38614b6833d9f018f0d7a76
Uzias1 <38708130+Uzias1@users.noreply.github.com>
2020-02-25 01:34:45 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pagina del dorime xD</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+	<center><embed src="multimedia/dorime.mp4" width="300" height="400" autostart="true" loop="true" volume="100"></embed></center>
+</body>
+</html>
